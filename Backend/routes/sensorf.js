87f5a57ef1c99d65dacf204cebfe0df8c3b4bdf5
anggasputraaaaa <26anggasaputra@gmail.com>
2024-07-31 20:29:04 +0800

    update vercel

---
+++ Backend/routes/sensorf.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const SensorData = require('../models/sensordataf');
+
+// Endpoint POST untuk menyimpan data sensor
+router.post('/data', async (req, res) => {
+  const { ultra, temperature, pressure, humidity, rainfall, wind,anemo,ldr, } = req.body;
+
+  const now = new Date();
+// Menambahkan 8 jam ke waktu UTC
+const utcOffset = now.getTime() + (now.getTimezoneOffset() * 60000);
+const witaTime = new Date(utcOffset + (8 * 3600000));
+
+// Mendapatkan tanggal dalam format DD/MM/YYYY
+const date = witaTime.toLocaleDateString('en-GB');
+
+// Mendapatkan waktu dalam format HH:MM:SS
+const time = witaTime.toTimeString().split(' ')[0];
+
+  try {
+    const newSensorData = new SensorData({
+      ultra,
+      temperature,
+      pressure,
+      rainfall,
+      wind,
+      anemo,
+      ldr,
+      humidity,
+      date,
+      time
+    });
+
+    await newSensorData.save();
+    res.status(201).send('Data saved successfully');
+  } catch (error) {
+    res.status(500).send('Error saving data: ' + error.message);
+  }
+});
+
+// Endpoint GET untuk mengambil data sensor
+router.get('/data', async (req, res) => {
+  try {
+    const data = await SensorData.find({});
+    res.json(data);
+  } catch (error) {
+    res.status(500).send('Error retrieving data: ' + error.message);
+  }
+});
+
+module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -3,19 +3,31 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const sensorRoutes = require('./routes/sensor');
+const sensorRoutesf = require('./routes/sensorf');
 require('dotenv').config(); // Tambahkan ini untuk memuat variabel lingkungan dari file .env
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI1 = process.env.MONGO_URI1;
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/anra', sensorRoutes);
+app.use('/fara', sensorRoutesf);
 
 // Koneksi ke MongoDB
 mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error.message);
+});
+
+mongoose.connect(MONGO_URI1, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
