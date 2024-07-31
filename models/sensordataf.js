73d3ee622a56200565664d987369ed41d7a48705
anggasputraaaaa <26anggasaputra@gmail.com>
2024-07-31 20:29:16 +0800

    update vercel

---
+++ models/sensordataf.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const SensorDataSchema = new mongoose.Schema({
+  ultra: Number,
+  temperature: Number,
+  pressure: Number,
+  rainfall: Number,
+  wind:Number,
+  anemo:Number,
+  ldr:Number,
+  humidity:Number,
+  date: { type: String, required: true },
+  time: { type: String, required: true }
+});
+
+module.exports = mongoose.model('SensorData', SensorDataSchema);
