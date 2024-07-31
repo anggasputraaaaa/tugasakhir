bb3b847c597f36dd13e1e4171ab3bedb4747ebb1
anggasputraaaaa <26anggasaputra@gmail.com>
2024-07-31 19:13:10 +0800

    update vercel

--- Frontend/src/components/Card.jsx
+++ Frontend/src/components/Card.jsx
@@ -19,7 +19,7 @@ const Card = () => {
     useEffect(() => {
         const fetchData = async () => {
           try {
-            const response = await axios.get('https://hydrometeorologi.vercel.app/api/data');
+            const response = await axios.get('https://hydrometeorologi.vercel.app/anra/data');
             response.data.map(item => (
                 setSuhu(item.temperature.toString()),
                 setKelembaban(item.humidity.toString()),
