44ec40cf39be078359dbe2e5a8c94e3ce3624f30
anggasputraaaaa <26anggasaputra@gmail.com>
2024-07-31 18:58:51 +0800

    update vercel

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ const MONGO_URI = process.env.MONGO_URI;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/angga', sensorRoutes);
+app.use('/anra', sensorRoutes);
 
 // Koneksi ke MongoDB
 mongoose.connect(MONGO_URI, {
