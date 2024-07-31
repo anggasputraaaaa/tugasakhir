46719bf02abe51761eaf54e627ef5ccbed367deb
anggasputraaaaa <26anggasaputra@gmail.com>
2024-07-31 20:48:51 +0800

    update

--- server.js
+++ server.js
@@ -18,22 +18,30 @@ app.use('/anra', sensorRoutes);
 app.use('/fara', sensorRoutesf);
 
 // Koneksi ke MongoDB
-mongoose.connect(MONGO_URI, {
+const db1 = mongoose.createConnection(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((error) => {
-  console.error('Error connecting to MongoDB:', error.message);
 });
 
-mongoose.connect(MONGO_URI1, {
+db1.on('connected', () => {
+  console.log('Connected to MongoDB database 1');
+});
+
+db1.on('error', (error) => {
+  console.error('Error connecting to MongoDB database 1:', error.message);
+});
+
+const db2 = mongoose.createConnection(MONGO_URI1, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((error) => {
-  console.error('Error connecting to MongoDB:', error.message);
+});
+
+db2.on('connected', () => {
+  console.log('Connected to MongoDB database 2');
+});
+
+db2.on('error', (error) => {
+  console.error('Error connecting to MongoDB database 2:', error.message);
 });
 
 // Jalankan server
